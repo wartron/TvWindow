eed786d13a9bd055774850f67f32493a5e9c85d7
Will Wharton <willmwharton@gmail.com>
2015-12-19 21:56:28 -0800

    moving stuff

---
+++ Assets/Scripts/SteroCamera/SteroCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteroCamera : MonoBehaviour {
+
+    public Camera cameraLeft;
+    public Camera cameraRight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
