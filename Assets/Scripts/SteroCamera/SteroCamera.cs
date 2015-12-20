5130262d6a12f2cb6dbb8b8b33e149373aa4bdba
Will Wharton <willmwharton@gmail.com>
2015-12-19 22:37:24 -0800

    simple spread test

--- Assets/Scripts/SteroCamera/SteroCamera.cs
+++ Assets/Scripts/SteroCamera/SteroCamera.cs
@@ -1,18 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SteroCamera : MonoBehaviour {
 
     public Camera cameraLeft;
     public Camera cameraRight;
 
+    public float defaultCameraSpread = 0.2f;
+    public float cameraSpread;
+    public bool flipEyes = false;
+
+    public float hScale = 0.005f;
+
+    public Text labelDepth;
+
 	// Use this for initialization
 	void Start () {
-	
+
+        cameraSpread = defaultCameraSpread;
+
+        adjustDepth();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float h = Input.GetAxis("Horizontal");
+
+        if (h != 0)
+        {
+            cameraSpread += h * hScale;
+
+            adjustDepth();
+        }
+
+
+
 	}
+
+
+    void adjustDepth()
+    {
+        float hs = cameraSpread / 2.0f;
+
+        float leftX = hs;
+        float rightX = hs;
+
+        if (flipEyes)
+        {
+            rightX *= -1;
+        }
+        else
+        {
+            leftX *= -1;
+
+        }
+
+
+        cameraLeft.transform.localPosition = new Vector3(leftX, 0, 0);
+        cameraRight.transform.localPosition = new Vector3(rightX, 0, 0);
+
+
+        labelDepth.text = string.Format("3D Depth: {0}", cameraSpread);
+
+
+    }
+
 }
